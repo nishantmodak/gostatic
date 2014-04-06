54b7793e8a4770b37211fa9d54f80605cfb33c38
Nishant Modak <modak.nishant@gmail.com>
2014-04-07 01:10:31 +0530

    improved static site

--- gostatic.go
+++ gostatic.go
@@ -2,9 +2,15 @@ package main
 
 import (
   "net/http"
+  "log"
 )
 
 func main() {
   fs := http.FileServer(http.Dir("public"))
-  http.ListenAndServe(":8080", fs)
+  http.Handle("/", fs)
+  log.Println("Listening...")
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
 }
